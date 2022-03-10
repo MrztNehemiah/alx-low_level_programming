71e76d8bffc4f510b18ef747eb22229d89bb9a56
Nehemiah Daniel <danielnehemiah15@gmail.com>
2022-03-10 05:42:43 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main -  entry point
+ *
+ * Return: Always 0 : (success)
+ */
+int main()
+{
+	put("/"Programming is like building multiline puzzle");
+
+	Return (0);
+
+}
